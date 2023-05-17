14ecd711b47f0320afd969b9ee78de1d0abfd43c
Filippsf <fafesas.filippos@gmail.com>
2023-05-17 21:26:34 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,38 @@
+const btnEl = document.getElementById("btn")
+const jokeEl = document.getElementById("joke")
+
+
+const apiKey = "PR08JYD2qxp16JGxiMV5jQ==9DUJ6IHOk53BB7XM"
+
+const options = {
+    method: "GET", 
+    headers: {
+        "X-Api-Key": apiKey,
+    },
+};
+
+const apiURL = "https://api.api-ninjas.com/v1/dadjokes?limit=1"
+
+async function getJoke() {
+    try {
+    jokeEl.innerText = "Updating...";
+    btnEl.disabled = true;
+    btnEl.innerText = "Loading...";
+    const response = await fetch(apiURL, options)
+    const data = await response.json();
+
+    btnEl.disabled = false;
+    btnEl.innerText = "Tell me a joke";
+
+    jokeEl.innerText = data[0].joke;
+
+    } catch (error) {
+        jokeEl.innerText = "An error had occured, try again later";
+        btnEl.disabled = false;
+        btnEl.innerText = "Tell me a joke";
+        console.log(error);
+    }
+}
+
+btnEl.addEventListener("click", getJoke)
+
